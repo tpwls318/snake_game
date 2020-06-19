25dba3750103d67adc0075bea19c41576532c348
박세진 <tpwls318@gmail.com>
2020-06-19 12:59:35 +0900

    implement crash, pause and showing score

--- snake.js
+++ snake.js
@@ -2,32 +2,47 @@ const col_length = 32;
 const row_length = 18;
 const EAST = { x: 1, y: 0 };
 const WEST = { x: -1, y: 0 };
-const NORTH = { x: 0, y: 1 };
-const SOUTH = { x: 0, y: -1 };
+const NORTH = { x: 0, y: -1 };
+const SOUTH = { x: 0, y: 1 };
 
+const getDistance = (p1, p2) => Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
 const randPos = ({ col, row }) => ({ x: Math.floor(Math.random() * col), y: Math.floor(Math.random() * row) });
+const randApplePos = state => {
+  let rand_pos = randPos(state);
+  if(!state.snake.reduce((acc, v) => getDistance(v, rand_pos) < 3 ? false : acc), true){
+    rand_pos = randPos(state);
+  }
+  return rand_pos;
+}
 const posEq = (p1, p2) => !(p1.x - p2.x || p1.y - p2.y);
 const willEat = (nextHead, apple) => posEq(nextHead, apple);
-const nextHead = ({ snake, dir }) => ({ x: snake[0].x + dir.x, y: snake[0].y + dir.y });
-const nextSnake = (state) => state.snake.unshift(nextHead(state)) && (willEat(nextHead(state), state.apple) || state.snake.pop()) && state.snake;
-const nextApple = state => willEat(nextHead(state), state.apple) ? randPos(state) : state.apple;
-// const nextDir = () => 0
+const nextHead = ({ snake, moves, col, row }) => ({ x: mod(snake[0].x + moves[0].x)(col), y: mod(snake[0].y + moves[0].y)(row) });
+const nextSnake = (state) => state.snake.unshift(nextHead(state))
+  && (willEat(state.snake[0], state.apple)
+  || state.snake.pop()) && state.snake;
+const nextApple = state => willEat(state.snake[0], state.apple) ? randApplePos(state) : state.apple;
+const invalidMove = (moves, dir) => moves[moves.length - 1].x && dir.x || moves[moves.length - 1].y && dir.y;
+const nextMoves = ({ moves }) => moves.length == 1 ? moves : moves.slice(1);
 
 const initialState = () => ({
   col: col_length,
   row: row_length,
   snake: [{ x: 2, y: 2 }],
   apple: { x: col_length - 3, y: 2 },
-  dir: EAST,
+  moves: [EAST],
+  pause: true,
 })
 const next = spec({
   col: x => x.col,
   row: x => x.row,
   snake: nextSnake,
   apple: nextApple,
-  dir: x => x.dir,
+  moves: nextMoves,
+  pause: x => x.pause,
 })
-const changeDirState = (dir, prev_state) => ({
-  ...prev_state,
-  dir,
-})
+const togglePause = (prev_state) => ({ ...prev_state, pause: !prev_state.pause })
+const enqueueMove = (prev_state, dir) => (
+  invalidMove(prev_state.moves, dir)
+    ? prev_state
+    : {...prev_state, moves: [...prev_state.moves, dir]}
+);
--- web.js
+++ web.js
@@ -1,45 +1,67 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
+let padding;
 let state = initialState();
-
+let last_score;
+let apple = document.getElementById("apple");
+apple.style.visibility = 'hidden';
 const x = p => p * canvas.width / state.col;
 const y = p => p * canvas.height / state.row;
+const getMessage = start => start ? 'press space to start' : `Your score :${last_score || state.snake.length}`
+const showMessage = (pause, t0) => {
+  document.getElementById('message').innerHTML = pause ? getMessage(!t0) : 'press space to pause';
+}
 const draw = () => {
+  // init last score
+  last_score = 0;
+
   // bgc
   ctx.fillStyle = '#94949c';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   
   // snake
   ctx.fillStyle = 'rgb(0, 200, 50)';
-  state.snake.forEach(b => ctx.fillRect(x(b.x), y(b.y), x(1), y(1)));
+  pd = state.col / 16;
+  state.snake.forEach(b => ctx.fillRect(x(b.x) + pd, y(b.y) + pd, x(1) - pd * 2, y(1) - pd * 2));
 
   // apple
-  ctx.fillStyle = 'rgb(225, 50, 0)';
-  ctx.fillRect(x(state.apple.x), y(state.apple.y), x(1), y(1));
+  var oc = document.createElement('canvas'), octx = oc.getContext('2d');
+  octx.drawImage(apple, 0, 0, x(1) - 6, y(1) - 6);
+  ctx.drawImage(oc, x(state.apple.x) + 3, y(state.apple.y) + 2);
 
   // crash
-  if(!state){
-    ctx.fillStyle = 'rgb(255, 40, 40)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (let i = 1; i < state.snake.length; i++) {
+    if(JSON.stringify(state.snake[0]) == JSON.stringify(state.snake[i])){
+      ctx.fillStyle = 'rgb(255, 40, 40)';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      window.setTimeout(() => {
+        ctx.fillStyle = '#94949c';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+      }, 100);
+      last_score = state.snake.length;
+      state = initialState();
+      break;
+    }
   }
 }
 const step = t0 => t1 => {
-  if(t1 - t0 > 100){
-    state = next(state);
-    draw();
-    window.requestAnimationFrame(step(t1));
-  } else {
+  showMessage(state.pause, t0);
+  if(state.pause || t1 - t0 < 100 - state.snake.length * 2) {
     window.requestAnimationFrame(step(t0));
+  } else {
+      state = next(state);
+      draw();
+      window.requestAnimationFrame(step(t1)); 
   }
 }
 
 window.addEventListener('keydown', e => {
   switch (e.key) {
-    case 'w': case 'ArrowUp':    state = state; break;
-    case 'a': case 'ArrowLeft':  state = state; break;
-    case 's': case 'ArrowDown':  state = state; break;
-    case 'd': case 'ArrowRight': state = state; break;
+    case 'w': case 'ArrowUp':    state = enqueueMove(state, NORTH); break;
+    case 'a': case 'ArrowLeft':  state = enqueueMove(state, WEST); break;
+    case 's': case 'ArrowDown':  state = enqueueMove(state, SOUTH); break;
+    case 'd': case 'ArrowRight': state = enqueueMove(state, EAST); break;
+    case ' ': case 'Escape': state = togglePause(state); break;
   }
 })
 
